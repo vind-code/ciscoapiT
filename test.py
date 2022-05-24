5862850b3978591caeff502d4d5a42b05d49d0dc
VND <vind@ngs.ru>
2022-05-24 20:38:25 +0700

    Import REST API Template

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
+import requests
 
 def main():
+
+name = 'student'
+
+pawd =  'Cisco123'
+
+url =  'https://192.168.0.229/doc'
+headers = {'Connection':  'Close',  'User-Agent':  'REST API Agent'}
+data = {'msid': phone, 'message': mess, 'naming': name, 'login': user, 'password': pamd5} 
+
+response = requests.post(url=url, data=data, headers=headers)
+
+if response:
+    print( 'Success!')
+else:
+    print( 'An error has occurred.')
 
 
 if __name__ == '__main__':
