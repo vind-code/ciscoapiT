9090f5e61ee0f8ff5ee78dd12157696af7dff274
unknown <C:\Users\VinD\AppData\Roaming\The Bat!>
2022-06-15 11:00:04 +0700

    New branch and new file, test class operations

--- test.py
+++ test.py
@@ -4,41 +4,40 @@ import os
 from requests.auth import HTTPBasicAuth
 import urllib3
 
-username = 'student'
-pawd = 'Cisco123'
-base_url = 'https://192.168.0.229/api/'
-
-def init_auth():
-    '''
+def init_auth(dic_dev):
+    """
     Base Auth and return device token
-    '''
+    """
+    url = 'https://{}/api/tokenservices'.format(dic_dev['ip'])
     headers = {'Content-type': 'application/json', 'User-Agent': 'REST API Agent'}
     try:
-        resp = requests.post(url = base_url + 'tokenservices', headers = headers, auth=HTTPBasicAuth(username, pawd), verify=False)
+        resp = requests.post(url = url, headers = headers, auth = HTTPBasicAuth(dic_dev['username'], dic_dev['password']), verify = False)
         return resp.headers['X-Auth-Token']
     except(ConnectionError): # Not good
         print('Failed to connect and authenticate to device. Check reachability, DNS, and/or username and password.\n')
         raise SystemExit
 
-def close_auth(xToken):
-    '''
+def close_auth(dic_dev,xToken):
+    """
     Destroy Token, return 0 if unsuccessful
-    '''
+    """
+    url = 'https://{}/api/tokenservices/{}'.format(dic_dev['ip'], xToken)
     headers = {'Content-type': 'application/json', 'User-Agent': 'REST API Agent'}
     try:
-        resp = requests.delete(url = base_url + 'tokenservices/' + xToken, headers = headers, auth=HTTPBasicAuth(username, pawd), verify=False)
+        resp = requests.delete(url = url, headers = headers, auth=HTTPBasicAuth(dic_dev['username'], dic_dev['password']), verify = False)
         return resp
     except(ConnectionError): # Not good
         print('Failed to destroy token.\n')
         raise SystemExit
 
-def get_int_counts(int_name,xToken):
-    '''
+def get_int_counts(dic_dev,int_name,xToken):
+    """
     Get interface monitoring info
-    '''
+    """
+    url = 'https://{}/api/monitoring/device/interfaces/{}'.format(dic_dev['ip'], int_name)
     headers = {'Content-type': 'application/json', 'User-Agent': 'REST API Agent', 'X-Auth-Token': xToken}
     try:
-        resp = requests.get(url = base_url + 'monitoring/device/interfaces/' + int_name, headers = headers, verify=False)
+        resp = requests.get(url = url, headers = headers, verify = False)
         return resp.content
     except(ConnectionError): # Not good
         print('Failed to destroy token.\n')
@@ -47,14 +46,15 @@ def get_int_counts(int_name,xToken):
 def main():
  
     urllib3.disable_warnings()
+    my_dev = {'username': 'student', 'password': 'Cisco123', 'ip': '192.168.0.229'}
 
-    Auth_Token = init_auth()
+    Auth_Token = init_auth(my_dev)
     ### print(Auth_Token)
-    mon_data = get_int_counts('inside',Auth_Token)
+    mon_data = get_int_counts(my_dev,'inside',Auth_Token)
     mon_dict = json.loads(mon_data)
     print('Input Byte Count: ', mon_dict['inputByteCount'])
     print('Output Byte Count: ', mon_dict['outputByteCount'])
-    close_auth(Auth_Token)
+    close_auth(my_dev,Auth_Token)
 
 if __name__ == '__main__':
     main()
---
+++ test_class.py
@@ -0,0 +1,86 @@
+from pickle import NONE
+import requests
+import json
+import os
+from requests.auth import HTTPBasicAuth
+import urllib3
+
+
+class asa:
+
+    def __init__(self, ip, username, password):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.token = None
+    
+
+    def auth_open(self):
+        """
+        Base Auth and return device token
+        """
+        url = 'https://{}/api/tokenservices'.format(self.ip)
+        headers = {'Content-type': 'application/json', 'User-Agent': 'REST API Agent'}
+        try:
+            resp = requests.post(url = url, headers = headers, auth = HTTPBasicAuth(self.username, self.password), verify = False)
+            self.token = resp.headers['X-Auth-Token']
+            return self.token
+        except(ConnectionError): # Not good
+            print('Failed to connect and authenticate to device. Check reachability, DNS, and/or username and password.\n')
+            raise SystemExit
+            
+
+    def auth_close(self):
+        """
+        Destroy Token, return 0 if unsuccessful
+        """
+        url = 'https://{}/api/tokenservices/{}'.format(self.ip, self.token)
+        headers = {'Content-type': 'application/json', 'User-Agent': 'REST API Agent'}
+        try:
+            resp = requests.delete(url = url, headers = headers, auth=HTTPBasicAuth(self.username, self.password), verify = False)
+            return resp
+        except(ConnectionError): # Not good
+            print('Failed to destroy auth token. Was it created by auth_open() function?\n')
+            raise SystemExit 
+        
+
+    def _get_s(self, api_uri):
+        """
+        Universal GET function. api 'tale' must be supplied - /api/...
+        """
+        url = 'https://{}{}'.format(self.ip, api_uri)
+        headers = {'Content-type': 'application/json', 'User-Agent': 'REST API Agent', 'X-Auth-Token': self.token} 
+        try:
+            resp = requests.get(url = url, headers = headers, verify = False)
+            return resp.content
+        except(ConnectionError): # Not good
+            print('Failed to GET from device.\n')
+            raise SystemExit
+
+
+    def get_int_info(self,int_name):
+        """
+        Get interface monitoring info. Correct interface name like 'inside' must be supplied
+        """
+        try:
+            return self._get_s('/api/monitoring/device/interfaces/{}'.format(int_name))
+        except(ConnectionError): # Not good
+            print('Failed to GET Interface info from Device. Check interface name.\n')
+            raise SystemExit
+
+
+
+def main():
+ 
+    urllib3.disable_warnings() # Disable 'untrusted certificate' warnings
+    my_dev = asa(ip ='192.168.0.229', username='student', password='Cisco123') # Create object type 'asa'
+    my_dev.auth_open() # Init auth to Device, get token
+    mon_data = my_dev.get_int_info('inside') # Get 'inside' interface Info
+    mon_dict = json.loads(mon_data) # Convert json to dictionary
+    print('Input Byte Count: ', mon_dict['inputByteCount'])
+    print('Output Byte Count: ', mon_dict['outputByteCount'])
+    my_dev.auth_close() # Destroy auth token, taken by auth_open() above
+
+
+if __name__ == '__main__':
+    main()
